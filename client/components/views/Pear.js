447629374b697389315b66b54cf87053c9d81c12
MichelleMullane <106880578+MichelleMullane@users.noreply.github.com>
2023-01-26 13:44:24 -0500

    converted Pear component to functional component

--- client/components/views/Pear.js
+++ client/components/views/Pear.js
@@ -1,19 +1,17 @@
-import React from 'react';
-import { NewItemForm } from '../forms';
-import { Table, BarChart } from './index';
+import React from "react";
+import { NewItemForm } from "../forms";
+import { Table, BarChart } from "./index";
 
-class Pear extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1 className="mb-5">Make your comPEARison 🍐</h1>
-        <h2>Enter Details for a New Item Below:</h2>
-        <NewItemForm />
-        <Table />
-        {/* <BarChart /> */}
-      </div>
-    );
-  }
-}
+const Pear = () => {
+  return (
+    <div>
+      <h1 className="mb-5">Make your comPEARison 🍐</h1>
+      <h2>Enter Details for a New Item Below:</h2>
+      <NewItemForm />
+      <Table />
+      <BarChart />
+    </div>
+  );
+};
 
 export default Pear;
